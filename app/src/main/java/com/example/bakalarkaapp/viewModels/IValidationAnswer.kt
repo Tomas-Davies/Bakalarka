71996e212bb4ded734bba96361165e2ef283e35d
Tomas-Davies <tomislav12345678900@gmail.com>
2025-03-03 22:12:57 +0100

    deleted

--- app/src/main/java/com/example/bakalarkaapp/viewModels/IValidationAnswer.kt
+++
@@ -1,7 +0,0 @@
-package com.example.bakalarkaapp.viewModels
-
-sealed interface IValidationAnswer {
-    data class StringAnswer(val value: String): IValidationAnswer
-    data class BooleanAnswer(val value: Boolean): IValidationAnswer
-    data object BlankAnswer : IValidationAnswer
-}
