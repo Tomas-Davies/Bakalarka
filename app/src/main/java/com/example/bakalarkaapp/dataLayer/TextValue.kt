34577667dd9dea895d03106a2c8695cd9bf43456
Tomas-Davies <114176024+Tomas-Davies@users.noreply.github.com>
2025-03-03 21:37:35 +0100

    Delete app/src/main/java/com/example/bakalarkaapp/dataLayer/TextValue.kt

--- app/src/main/java/com/example/bakalarkaapp/dataLayer/TextValue.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bakalarkaapp.dataLayer
-
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlText
-
-class TextValue {
-    @JacksonXmlText
-    val value: String = ""
-}
